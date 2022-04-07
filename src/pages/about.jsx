3801db60ae155869cec411c31429f4328d43f2f8
aqib7275 <aqib@kodifly.com>
2022-04-07 12:05:09 +0500

    about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>KAJS COASID</p>
+      <p>KAJS COASIDOK KASKDM APM DPAAA</p>
+    </div>
+  );
+};
+
+export default about;
